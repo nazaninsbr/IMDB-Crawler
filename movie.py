897d048621179a9588ebbe0b364e3ae951845f77
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-08-09 11:48:34 +0430

    add rating to movies

--- movie.py
+++ movie.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import copy
-
+from rating import Rating 
 
 class Movie:
-    def __init__(self, title='', director='', actors=[]):
+    def __init__(self, title='', director='', actors=[], rating_information={}):
         self.title = title
         self.director = director
         self.actors = actors
+        self.rating = Rating(rating_information)
 
     def __str__(self):
         return self.title + '#' + self.director + '\n'
@@ -24,3 +25,11 @@ class Movie:
     def parse(cls, string):
         parsed = string.strip().split('#')
         return Movie(parsed[0], parsed[1])
+
+    def get_rating(self):
+        return str(self.rating)
+
+    
+
+    
+    
